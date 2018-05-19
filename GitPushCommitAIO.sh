2760e2155d25be37a8faa0fd5abc07a2e95c05c0
wandyme <wandyme@gmail.com>
2018-05-19 19:26:25 +0800

    Create GitPushCommitAIO.sh
    This is for git push the all in one LabVIEW VIs.

---
+++ GitPushCommitAIO.sh
@@ -0,0 +1,7 @@
+# cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/ALL\ IN\ ONE_LV12_WDY
+cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/D&W-Control_LV12_WDY
+git status
+git add .
+git commit -m "update revision history"
+#git log
+git push -u origin master
