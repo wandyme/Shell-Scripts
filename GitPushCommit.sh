0ff34bc488632dee35d495cc43312dd3e2ba309a
wandyme <wandyme@gmail.com>
2018-05-19 19:37:43 +0800

    test add description
    description is added

--- GitPushCommit.sh
+++ GitPushCommit.sh
@@ -1,7 +1,11 @@
-# cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/ALL\ IN\ ONE_LV12_WDY
-cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/D&W-Control_LV12_WDY
+#cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/ALL\ IN\ ONE_LV12_WDY
+#cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/D\&W-Control_LV12_WDY
+
 git status
 git add .
-git commit -m "update revision history"
+git commit -m "test add description" -m "description is added"
 #git log
-git push -u origin master
+#git push -u origin master
+
+
+#./Git\ push\ commit.sh
--- GitScriptTest.sh
+++ GitScriptTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Hello World !"
 
-echo -e "tast\ntest"
+echo  "tast\ntest"
