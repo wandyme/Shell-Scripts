bf18d4a6debe9611064fee3b68aca9c76737909e
wandyme <wandyme@gmail.com>
2018-05-23 20:37:58 +0800

    add GitPushCommitDWC.sh

---
+++ GitPushCommitDWC.sh
@@ -0,0 +1,8 @@
+# git push "all in one" labview VIs.
+# cd /Users/wandy/OneDrive/T-Lab/[My\ Projects\&Data]/LabVIEW\ VIs/Fs\ Lab\ VIs/ALL\ IN\ ONE_LV12_WDY
+cd "/Users/wandy/OneDrive/T-Lab/[My Projects&Data]/LabVIEW VIs/Fs Lab VIs/D&W-Control_LV12_WDY"
+git status
+git add . #--all
+git commit -m "Ver. 2.0.1" -m "fix a bug"
+#git log
+git push -u origin master
