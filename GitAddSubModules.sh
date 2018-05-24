1c5a6a88f50487e5617147542d2995b975774d22
wandyme <wandyme@gmail.com>
2018-05-24 22:40:40 +0800

    add GitAddSubModules.sh

---
+++ GitAddSubModules.sh
@@ -0,0 +1,6 @@
+cd "/Users/wandy/OneDrive/T-Lab/[My Projects&Data]/LabVIEW VIs/Fs Lab VIs/ALL IN ONE_LV12_WDY" #"Your path"
+git submodule add https://github.com/wandyme/Fs-Lab-Shared-SubVIs.git
+git status
+git add --all
+git commit -m "Add submodules"
+git push
