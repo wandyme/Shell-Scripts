5f80e8501474d73ebacb79a444699aeb6aac545c
wandyme <wandyme@gmail.com>
2018-05-25 22:17:08 +0800

    add the GitPushSubModules.sh

---
+++ GitPushSubModules.sh
@@ -0,0 +1,12 @@
+cd "/Users/wandy/OneDrive/T-Lab/[My Projects&Data]/LabVIEW VIs/Fs Lab VIs/ALL IN ONE_LV12_WDY"
+git status
+git sf 'git branch' # sf for submodule foreach
+git sf 'git checkout master'
+git sf 'git status'
+git sf 'git add .'
+git sf 'git commit -m "6st test"'
+git sf 'git push'
+git status
+git add .
+git commit -m "6st test"
+git push
